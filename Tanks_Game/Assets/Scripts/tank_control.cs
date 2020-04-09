051f9ef7b8c1bf89f748106a1522b44d5096e617
karoljanic <karoljanic@gmail.com>
2020-04-09 20:00:37 +0200

    clean useless elements in project

--- Tanks_Game/Assets/Scripts/tank_control.cs
+++ Tanks_Game/Assets/Scripts/tank_control.cs
@@ -9,9 +9,7 @@ public class tank_control : MonoBehaviour
     private Rigidbody2D rb;
     private float speed = 100;
     private float movement;
-    private float getRifleUp;
-    public Transform rifle; 
-  
+
     [SerializeField]
     private bool facingRight = true;
     Vector3 localScale;
@@ -25,33 +23,25 @@ public class tank_control : MonoBehaviour
     void Update()
     {
         movement = CrossPlatformInputManager.GetAxis("Horizontal") * speed;
-
-        CheckIfRifleUp();
     }
 
     private void LateUpdate()
     {
-         check_where_to_face();
+        check_where_to_face();
     }
 
     void check_where_to_face()
     {
-         if (movement > 0)
-             facingRight = true;
-         else if (movement < 0)
-             facingRight = false;
+        if (movement > 0)
+            facingRight = true;
+        else if (movement < 0)
+            facingRight = false;
 
-         if (((facingRight) && (localScale.x < 0)) || ((!facingRight) && (localScale.x > 0)))
-             localScale.x *= -1;
+        if (((facingRight) && (localScale.x < 0)) || ((!facingRight) && (localScale.x > 0)))
+            localScale.x *= -1;
 
-         transform.localScale = localScale;
+        transform.localScale = localScale;
     }
-    
-    void CheckIfRifleUp()
-    {
-        if (CrossPlatformInputManager.GetButton("button_fire"))
-            rifle.rotation = Quaternion.Euler(0, 0, 90 * localScale.x);
-        else
-            rifle.rotation = Quaternion.Euler(0, 0, 23);
-    }
+
+ 
 }
---
+++ Tanks_Game/Assets/Scripts/turret_control.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class turret_control : MonoBehaviour
+{
+
+    private void Update()
+    {
+        Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        float rotZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+        if (rotZ < -12)
+            rotZ = -12;
+        else if (rotZ > 140)
+            rotZ = 140;
+        transform.rotation = Quaternion.Euler(0f, 0f, rotZ);
+    }
+
+}
