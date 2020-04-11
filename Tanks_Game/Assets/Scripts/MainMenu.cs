b8da1c6a82c6793991501d07bbb13212517de4a2
karoljanic <karoljanic@gmail.com>
2020-04-11 23:02:34 +0200

    add main menu

---
+++ Tanks_Game/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public Sprite tank;
+    public Sprite[] sprites;
+    private int i = 0;
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void ExitGame()
+    {
+        Debug.Log("Exit");
+        Application.Quit();
+    }
+}
