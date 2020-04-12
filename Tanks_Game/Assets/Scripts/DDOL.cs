246b7ba59fe990c9a91f974cbcdb8feafb58d0a2
karoljanic <karoljanic@gmail.com>
2020-04-13 01:28:46 +0200

    multiplayer first stage

---
+++ Tanks_Game/Assets/Scripts/DDOL.cs
@@ -0,0 +1,10 @@
+﻿
+using UnityEngine;
+
+public class DDOL : MonoBehaviour
+{
+    private void Awake()
+    {
+        DontDestroyOnLoad(this);
+    }
+}
---
+++ Tanks_Game/Assets/Scripts/RoomLayoutGroup.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomLayoutGroup : MonoBehaviour
+{
+    [SerializeField]
+
+    private GameObject _roomListingPrefab;
+    private GameObject RoomListingPrefab
+    {
+        get { return _roomListingPrefab; }
+    }
+
+    private List<RoomListing> _roomListingButtons = new List<RoomListing>();
+    private List<RoomListing> RoomListingsButtons
+    {
+        get { return _roomListingButtons; }
+    }
+
+    private void OnReceivedRoomListUpdate()
+    {
+        RoomInfo[] rooms = PhotonNetwork.GetRoomList();
+        foreach (RoomInfo room in rooms)
+        {
+            RoomReceived(room);
+        }
+        RemoveOldRooms();
+    }
+
+    private void RoomReceived(RoomInfo room)
+    {
+        int index = RoomListingsButtons.FindIndex(x => x.RoomName == room.Name);
+
+        if(index == -1)
+        {
+            if(room.IsVisible && room.PlayerCount<room.MaxPlayers)
+            {
+                GameObject roomListingObj = Instantiate(RoomListingPrefab);
+                roomListingObj.transform.SetParent(transform, false);
+
+                RoomListing roomListing = roomListingObj.GetComponent<RoomListing>();
+                RoomListingsButtons.Add(roomListing);
+
+                index = (RoomListingsButtons.Count - 1);
+            }
+        }
+
+        if(index !=-1)
+        {
+            RoomListing roomListing = RoomListingsButtons[index];
+            roomListing.SetRoomNameText(room.Name);
+            roomListing.Update = true;
+        }
+    }
+
+    private void RemoveOldRooms()
+    {
+        List<RoomListing> removeRooms = new List<RoomListing>();
+
+        foreach(RoomListing roomListing in RoomListingsButtons)
+        {
+            if (!roomListing.Update)
+                removeRooms.Add(roomListing);
+            else
+                roomListing.Update = false;
+        }
+
+        foreach(RoomListing roomListing in removeRooms)
+        {
+            GameObject roomListingObj = roomListing.gameObject;
+            RoomListingsButtons.Remove(roomListing);
+            Destroy(roomListingObj);
+        }
+    }
+}
